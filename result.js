91aebcee79e8b87b2be47c43d5dbc542b4c49dd0
Cosimo0C <cosimocrupi01@gmail.com>
2025-04-03 12:36:19 +0200

    result

--- result.js
+++ result.js
@@ -1,6 +1,12 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const numDom = Number(urlParams.get("a"));
+const risCor = Number(urlParams.get("b"));
+
 function calcoloPercentuale() {
-  const massimo = 100;
-  const randomGiuste = Math.floor(Math.random() * massimo + 1);
+  const massimo = numDom;
+  const randomGiuste = risCor;
   console.log(randomGiuste);
 
   const randomSbagliate = massimo - randomGiuste;
@@ -31,13 +37,13 @@ function calcoloPercentuale() {
   const aswers2 = document.querySelector(".comment-test h5");
   console.log(aswers);
   console.log(aswers2);
-  if (randomGiuste < 60) {
+  if (randomGiuste < (massimo * 60) / 100) {
     aswers.innerText = "Failed!";
     aswers2.innerText = "You don't passed the exam.";
     aswers2.style.color = "#d20094";
   }
   const agata = document.querySelector(".meta1");
   let value = (randomSbagliate / massimo) * 360;
-  agata.style.background = `conic-gradient( #d20094 0deg ${value}deg, cyan 0deg 360deg)`;
+  agata.style.background = conic-gradient( #d20094 0deg ${value}deg, cyan 0deg 360deg);
 }
 calcoloPercentuale();
